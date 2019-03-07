8d02701c66e3daf6822722973cf6e334982a9508
Thomaz Cortez <thomazfcortez@gmail.com>
2019-03-06 22:23:28 -0300

    Server Engine delegated to default Ktor Netty configuration

--- src/Application.kt
+++ src/Application.kt
@@ -6,32 +6,14 @@ import io.ktor.jackson.jackson
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.server.engine.ApplicationEngineEnvironment
-import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.connector
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.netty.EngineMain
 
 @Suppress("unused") // Referenced in application.conf
-fun main(args: Array<String>) {
-    val server = Netty.create(loadEnvironmentConfiguration()) {
+fun main(args: Array<String>) = EngineMain.main(args)
 
-    }
-
-    server.application.apply {
-        installDependencies()
-        defineRoutes()
-    }
-
-    server.start(wait = true)
-}
-
-fun loadEnvironmentConfiguration(): ApplicationEngineEnvironment {
-    return applicationEngineEnvironment {
-        connector {
-            port = System.getenv("PORT").toInt()
-        }
-    }
+fun Application.module() {
+    installDependencies()
+    defineRoutes()
 }
 
 fun Application.installDependencies() {
