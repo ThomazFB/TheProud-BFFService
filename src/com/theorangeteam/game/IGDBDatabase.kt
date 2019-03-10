604fde409c56aa99b50066ef27bfdcf06107e0c6
Thomaz Cortez <thomazfcortez@gmail.com>
2019-03-10 03:39:27 -0300

    Request handling refactoring

--- src/com/theorangeteam/game/IGDBDatabase.kt
+++ src/com/theorangeteam/game/IGDBDatabase.kt
@@ -1,16 +1,27 @@
 package com.theorangeteam.game
 
-import Endpoint
-import IGDBWrapper.apiJsonRequest
+import IGDBWrapper
 import com.theorangeteam.contract.GameDatabase
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.headers
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import kotlinx.coroutines.runBlocking
 
 class IGDBDatabase : GameDatabase {
 
+    val httpClient by lazy { HttpClient(Apache) }
+
     override fun requestGameJsonListFromQuery(query: String): String {
-        try {
-            return apiJsonRequest(Endpoint.GAMES, query)
-        } catch (ex: Exception) {
-            return ex.localizedMessage
+        return runBlocking {
+            httpClient.post<String>("https://api-v3.igdb.com/games", block = {
+                headers {
+                    set("user-key", IGDBWrapper.userkey)
+                    set("Accept", "application/json")
+                }
+                parameter("fields", "name,cover")
+            })
         }
     }
 }
